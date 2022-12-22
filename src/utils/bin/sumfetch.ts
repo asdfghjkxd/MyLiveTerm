3167e0d42187d392b9e354a821ca2650975cedbe
George <73705042+asdfghjkxd@users.noreply.github.com>
2022-12-22 22:37:55 +0800

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -14,7 +14,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
 @@             .@@@@@@@@@@.          @@      CONTACT 
  @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
   @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+   @@             @@@@@@          @@         <u><a href="https://linkedin.com/${config.social.linkedin}" target="_blank">linkedin.com/${config.social.linkedin}</a></u>
      @@@                        @@@          <u><a href="https://${config.social.dockerhub}" target="_blank">${config.social.dockerhub}</a></u>
         @@@                  @@@ @@         -----------
          @|  @@@@@@@@@@@@@@@@   @@     
